688b8b384084e9c117b0b2ecc2d94ee92342611b
Ethan Klein <ethan@niceness.org>
2018-11-24 14:08:41 -0800

    added a few more vehicle makes and better error message

--- src/app/vehicle-form/vehicle-form.component.ts
+++ src/app/vehicle-form/vehicle-form.component.ts
@@ -9,8 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class VehicleFormComponent implements OnInit {
   vehicleId = "balkj";
-  makes = ['CHEV', 'SUBA',
-            'CHRY', 'TOYT'];
+  makes = ['CHEV', 'SUBA', 'CHRY', 'TOYT', 'MERZ', 'BMW'];
 
   model = new Vehicle('CHEV', 'volt', '2017', 'black');
   submitted = false;
@@ -23,12 +22,14 @@ export class VehicleFormComponent implements OnInit {
   shotCode = '037';
   apiKey = '06b3ad37-40ec-4466-9da4-dba806d212f1';
   stringAtEndOfImageUrl = "?fuel_app_key=0f615c30c3534a95a20f3ff8e313e0e6&Expires=1542386307&Signature=YJc8Af3CQt7cEKmznvJhw3U1y7BlNXpZaSZ-z3Ak8GQXlSx9rC6houQ4XJvus2wmyjXgNUt-q0ahIZkTqPCCOPKRcK72FF4z6lgtNOobGlkqtpoXNW4t0k3QfQ9fUAxwIgqi7U4pWSnMjOZcpHJ8TWRSlqj1AYGlrOgfd5uoQZFEQ0kNhw-qOXXxh4Nu7VZeZTAIKxnVAu29oCzSshn41PWvYftc8JSY5NJFBxsO5fRasBp-zm71ueV~BabSg2xcCnAzaXV1DKX9ucZfODNWxfcYrThYLhLwDuzVXvIWoahMoaeMXv24-igEHjFOsUb4AdLhkjppD9tkhStPlArQgw__&Key-Pair-Id=APKAJEHLXC3UBWDEGR3A";
-  imageUrl = "https://i.fuelapi.com/0f615c30c3534a95a20f3ff8e313e0e6/26004/2/6/color_0640_001_png/MY2017/11163/11163_cc0640_001_G1W.png";
+  imageUrl = "https://i.fuelapi.com/0f615c30c3534a95a20f3ff8e313e0e6/26004/2/12/color_0640_032_png/MY2017/11163/11163_cc0640_032_GAZ.png";
   vehicleShortNameLookupTable = [
                                   {shortname: 'CHEV', longname: 'Chevrolet'},
                                   {shortname: 'SUBA', longname: 'Subaru'},
                                   {shortname: 'CHRY', longname: 'Chrysler'},
-                                  {shortname: 'TOYT', longname: 'Toyota'}
+                                  {shortname: 'TOYT', longname: 'Toyota'},
+                                  {shortname: 'MERZ', longname: 'Mercedes-Benz'},
+                                  {shortname: 'BMW', longname: 'BMW'}
                                 ];
 
 
